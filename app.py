aaf31d2055b3f8ac421e5b192e047e9c84f37b89
Avi <neuramachine@gmail.com>
2023-06-11 09:55:01 +0300

    V2 no ST

--- app.py
+++ app.py
@@ -1,47 +1,35 @@
-import streamlit as st
 from langchain.llms.openai import OpenAI
 from langchain import PromptTemplate
-from dotenv import dotenv_values
+from dotenv import load_dotenv
+import os
 
-config = dotenv_values(".env")
-openai_api_key = config["OPENAI_KEY"]
+# Load environment variables
+load_dotenv()
 
-template = """
+# Get the OpenAI key from the environment
+openai_api_key = os.getenv("OPENAI_KEY")
+
+prompt_template = """
 1. Analyzing the article writing style
-2. rewrite it in hebrew. use "new information" to generate the new one.
+2. generate the new one in hebrew.
 
 "article":
 {article}
 
-"new information":
-{info}
 """
 
-# Streamlit app
-st.title('LangChain Text Summarizer')
+input_article = """  PUT YOUR ARTICLE HERE """
 
-source_text = st.text_area("Source Text", height=200)
-keywords_text = st.text_area("keywords data", height=200)
+# Initialize the OpenAI module, load and run the summarize chain
+llm = OpenAI(model_name="gpt-3.5-turbo", temperature=0, openai_api_key=openai_api_key)
 
-# Check if the 'Summarize' button is clicked
-if st.button("Summarize"):
-    # Validate inputs
-    if not openai_api_key.strip() or not source_text.strip() or not keywords_text:
-        st.error(f"Please provide the missing fields.")
-    else:
-        try:
+prompt = PromptTemplate(
+    input_variables=["article"],
+    template=prompt_template,
+)
 
-            # Initialize the OpenAI module, load and run the summarize chain
-            llm = OpenAI(model_name="gpt-3.5-turbo", temperature=0, openai_api_key=openai_api_key)
+final_prompt = prompt.format(article=input_article)
 
-            prompt = PromptTemplate(
-                input_variables=["article", "info"],
-                template=template,
-            )
+output = llm(final_prompt)
 
-            final_prompt = prompt.format(article=source_text, info=keywords_text)
-
-            # Display summary
-            st.success(llm(final_prompt))
-        except Exception as e:
-            st.error(f"An error occurred: {e}")
+print(output)
